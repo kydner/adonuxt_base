4e55294753281a1fde7ca85c3afb8020f7a1b4c9
Rendika <Programmer@DESKTOP-E2VB4C7>
2020-07-30 14:26:02 +0700

    no-ssr

--- config/nuxt.js
+++ config/nuxt.js
@@ -113,8 +113,9 @@ module.exports = {
    ** Plugins to load before mounting the App
    */
   plugins: [
-    // { src: '~/plugins/vuesax', ssr: false },
-    { src: '~/plugins/vue-bootstrap', ssr: false }
+    '~/plugins/vue-bootstrap',
+    '~/plugins/no-ssr'
+    // { src: '~/plugins/vue-bootstrap', ssr: false }
   ],
   router: {
     base: '/',
--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <section class="container">
+  <div>
+    <section class="container">
     <img src="/img/logo.png" alt="Adonuxt logo"/>
     <h1>Universal Vue.js Applications Framework</h1>
     <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
-    <vs-button color="warning" type="filled">Warning</vs-button>
-    <vs-button color="danger" type="relief">Danger</vs-button>
+    <b-button variant="primary">test</b-button>
   </section>
+  </div>
 </template>
 
 <script>
