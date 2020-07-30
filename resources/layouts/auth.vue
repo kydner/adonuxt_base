9a2df88ac29564a48cc2eda11a3929fbdd9dbe42
Rendika <Programmer@DESKTOP-E2VB4C7>
2020-07-30 10:24:31 +0700

    add images

--- resources/layouts/auth.vue
+++ resources/layouts/auth.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    auth
-    <nuxt/>
-  </div>
+  <b-container fluid>
+    <b-row align-v="center">
+      <b-col cols="6" class="left-side px-4 py-3">
+        <h2 style="color:#fefefe;">test 123</h2>
+      </b-col>
+      <b-col cols="6" class="right-side px-4 py-3">
+        <nuxt/>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+<script>
+export default {
+  
+}
+</script>
+<style scopped>
+  .left-side {
+    background-image:url('~/assets/images/background-login.jpg');
+    height: 100vh;
+  }
+  .right-side {
+    height: 100vh;
+  }
+</style>
